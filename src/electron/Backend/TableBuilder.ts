b2ccf42c39fdd1e06ca55b2c38d91b8d62627f58
tobiaschisi <tobias.chisi@gmail.com>
2024-11-30 16:18:46 +0100

    Working on inputing data in TableData type

--- src/electron/Backend/TableBuilder.ts
+++ src/electron/Backend/TableBuilder.ts
@@ -2,42 +2,45 @@ import TableData from "./Interfaces/TableData.js";
 import TableSchema from "./Interfaces/TableSchema.js";
 import JsonObject from "./Interfaces/JsonObject.js";
 class TableBuilder {
+  private tableDate: TableData[] = [];
+  private foreignIndex: number = 0;
+  private mainIndex: number = 0;
   public build(json: JsonObject[], tableSchema: TableSchema): TableData[] {
-    let result: TableData[] = [];
-
     Object.keys(tableSchema).forEach((key: string) => {
-      result.push({
+      tableSchema[key].push("id");
+      this.tableDate.push({
         schema: { [key]: tableSchema[key] },
         table: [],
       });
     });
 
-    console.log("TableData", result);
+    console.log("TableData", JSON.stringify(this.tableDate, null, 2));
+    json.forEach((object) => {
+      this.recursive(object, "main_table");
+    });
+
+    return this.tableDate;
+  }
+
+  private recursive(json: JsonObject, tableName: string) {
+    let row: { key: string; value: string | number; tableName: string }[] = [];
+
     Object.keys(json).forEach((key) => {
-      // Reference to table data object n results array
-      const tableData = result.find((table) => {
-        table.schema.hasOwnProperty(key);
-      });
-      if (tableData) {
-        // tableData.table.push(this.recursive(json, result, 0));
+      if (Array.isArray(json[key])) {
+        json[key].forEach((values) => {
+          row.push({ key: key, value: this.foreignIndex++, tableName });
+          this.recursive(values, key);
+        });
+      } else {
+        row.push({ key: key, value: json[key], tableName: tableName });
       }
     });
 
-    return result;
+    this.incertRow(row!);
   }
 
-  private recursive(json: JsonObject, result: TableData[], idIndex: number) {
-    let collector: TableData[];
-    Object.keys(json).forEach((key) => {
-      if (Array.isArray(json[key])) return;
-      // collector.push(...this.recursiveHelper(json[key], result, idIndex));
-    });
-  }
-
-  private recursiveHelper(
-    json: JsonObject[],
-    result: TableData[],
-    idIndex: number
+  private incertRow(
+    row: { key: string; value: string | number; tableName: string }[]
   ) {}
 }
 export default TableBuilder;
