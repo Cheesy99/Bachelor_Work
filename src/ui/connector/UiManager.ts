bc81086751bd1f2dcdb1414d33804512af6a6fd9
tobiaschisi <tobias.chisi@gmail.com>
2025-01-11 14:50:23 +0100

    Git is broken

--- src/ui/connector/UiManager.ts
+++ src/ui/connector/UiManager.ts
@@ -86,6 +86,11 @@ class UiManager {
     }
   }
 
+  public async deleteRow(id: number, tableName: string): Promise<void> {
+    const command: string = `SELECT * FROM ${tableName} WHERE id != ${id}`;
+    await window.electronAPI.sendSqlCommand(command, tableName);
+  }
+
   public async executeColumnFilter(
     selectedValues: (string | number)[],
     columnName: string
